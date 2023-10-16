378b4d9739be45ec6afbe107f73f91e4a0f126c9
Alexedpx <alexandradepourtoux93@gmail.com>
2023-10-16 12:36:14 +0200

    exercice de card css, avec react

---
+++ Card/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import './components/style.scss';
+
+function App() {
+
+
+  return (
+  <div class="container-card">
+  <img src="https://i.pinimg.com/originals/df/63/d1/df63d17365017f40a86e9ef05265af85.jpg"/>
+    <h1>☀️ SunFlower ☀️</h1>
+     <p> Le tournesol, également connu sous les noms hélianthe et soleil, est une grande plante annuelle, appartenant à la famille des Astéracées, dont les fleurs sont groupées en capitules de grandes dimensions.</p>
+    
+     <div class="entretien"> 
+     Eau : 💧💧 
+    </div>
+    </div>
+  )
+}
+
+export default App
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React-card-css
+
+J'ai choisi d'utiliser la méthode avec le style.scss car je trouve que c'est la plus simple d'utilisation et celle qui se rapproche le plus de ce que l'on a déjà pratiqué.
